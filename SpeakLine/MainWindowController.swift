7f8cbdd55a8acef88aced233b586163dc5c2a461
Stephen Skubik-Peplaski <codesmith@me.com>
2015-09-12 13:04:57 -0400

    Make a single window app

---
+++ SpeakLine/MainWindowController.swift
@@ -0,0 +1,23 @@
+//
+//  MainWindowController.swift
+//  SpeakLine
+//
+//  Created by Stephen Skubik-Peplaski on 9/12/15.
+//  Copyright © 2015 Stephen Skubik-Peplaski. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+    override var windowNibName: String {
+        return "MainWindowController"
+    }
+    
+}
