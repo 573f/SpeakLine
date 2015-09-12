d94df665687d343a44177fde36d6806e62fd3075
Stephen Skubik-Peplaski <codesmith@me.com>
2015-09-12 12:45:57 -0400

    Initial Commit

---
+++ SpeakLine/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SpeakLine
+//
+//  Created by Stephen Skubik-Peplaski on 9/12/15.
+//  Copyright © 2015 Stephen Skubik-Peplaski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
